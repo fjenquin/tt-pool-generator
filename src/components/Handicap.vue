e5df7d54a2c3e5254c6c82e02806530f4db93883
fjenquin <fred.jenquin@gmail.com>
2018-05-21 20:16:45 +0200

    Corrected handicap calculation

--- src/components/Handicap.vue
+++ src/components/Handicap.vue
@@ -14,11 +14,14 @@ export default {
     handicap: function() {
       let val =
         rankings.indexOf(this.player2Ranking) - rankings.indexOf(this.player1Ranking);
+      if (val === 0) {
+        return 0;
+      }
       let multiplier = 1;
       if (val < 0) {
         multiplier = -1;
       }
-      val = Math.floor((val + multiplier) / 2);
+      val = Math.floor((val + 2 * multiplier) / 2);
       if (val * multiplier > 8) {
         val = multiplier * 8;
       }
